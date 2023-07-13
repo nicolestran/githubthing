71704cd4ceec5e6bb80c362627847d415b115feb
nicole tran <nicolet2090@gmail.com>
2023-07-13 11:26:07 -0700

    Initial Commit

---
+++ githubthing/githubthingApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubthingApp.swift
+//  githubthing
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct githubthingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
